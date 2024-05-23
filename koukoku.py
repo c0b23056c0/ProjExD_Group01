525bdf292ad85b05ca2b6ac6e0d267a2bd2a3165
Takuya <c0b23056c0@edu.teu.ac.jp>
2024-05-23 17:23:29 +0900

    ピンの実装：完了

--- koukoku.py
+++ koukoku.py
@@ -1,32 +1,109 @@
 import os
+from pygame.locals import *
 import sys
 import pygame as pg
 
+pg.init()
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
+
+WIDTH, HEIGHT = 600, 900
+
+
+def check_bound(obj_rct:pg.Rect) -> tuple[bool, bool]:
+    """
+    Rectの画面内外判定用の関数
+    引数：こうかとんRect，または，爆弾Rect，またはビームRect
+    戻り値：横方向判定結果，縦方向判定結果（True：画面内／False：画面外）
+    """
+    yoko, tate = True, True
+    if obj_rct.left < 0 or WIDTH < obj_rct.right:  # 横方向のはみ出し判定
+        yoko = False
+    if obj_rct.top < 0 or HEIGHT < obj_rct.bottom:  # 縦方向のはみ出し判定
+        tate = False
+    return yoko, tate
+
+
+class Yoko_Pin(pg.sprite.Sprite): # 横用のピンのクラス
+    yoko_topleft_xy = [(50, 165), (50, 290), (300, 165), (300, 290)] # ピンの座標タプルのリスト
+
+    def __init__(self, ytl):
+        super().__init__()
+        self.image = pg.Surface((250, 20)) # ピンの横用のSurface
+        pg.draw.rect(self.image, (255, 215, 0), [0, 0, 250, 20])
+        pg.draw.rect(self.image, (0, 0, 0), [0, 0, 250, 20], 2) # ピンの枠線
+        self.rect = self.image.get_rect()
+        # ステージごとに配置を変えれるようにする
+        self.rect.topleft = __class__.yoko_topleft_xy[ytl]
+        self.vx, self.vy = 0, 0
+
+    def update(self):
+        # マウスの処理
+        if pg.mouse.get_pressed()[0]:
+            mouse_x, mouse_y = pg.mouse.get_pos() # マウスのｘ座標とｙ座標の取得
+            if self.rect.centerx > (WIDTH / 2): # ピンが画面の右側にあったら
+                if (self.rect.centerx <= mouse_x <= self.rect.right and self.rect.top <= mouse_y <= self.rect.bottom): # マウスがピンの半分より右側にあったら
+                    self.vx = +2
+            elif self.rect.centerx < (WIDTH / 2): # ピンが画面の左側にあったら
+                if (self.rect.left <= mouse_x <= self.rect.centerx and self.rect.top <= mouse_y <= self.rect.bottom): # マウスがピンの半分より左側にあったら
+                    self.vx = -2
+        
+        self.rect.move_ip(self.vx, self.vy)
+        if check_bound(self.rect) != (True, True):
+            self.kill() # 画面外にでたらGroupオブジェクトから消去される
+
+
+class Tate_Pin(pg.sprite.Sprite): # 縦用のピンのクラス
+    tate_topleft_xy = [(290, 50), (290, 600)] # 座標タプルのリスト
+
+    def __init__(self, ttl):
+        super().__init__()
+        self.image = pg.Surface((20, 250)) # ピンの縦用のSurface
+        pg.draw.rect(self.image, (255, 215, 0), [0, 0, 20, 250])
+        pg.draw.rect(self.image, (0, 0, 0), [0, 0, 20, 250], 2)
+        self.rect = self.image.get_rect()
+        # ステージごとに変えることのできる配置
+        self.rect.topleft = __class__.tate_topleft_xy[ttl]
+        self.vx, self.vy = 0, 0
+
+    def update(self):
+        if pg.mouse.get_pressed()[0]: # マウスの処理
+            mouse_x, mouse_y = pg.mouse.get_pos()
+            if self.rect.centery > (HEIGHT / 2):
+                if (self.rect.left <= mouse_x <= self.rect.right and self.rect.centery <= mouse_y <= self.rect.bottom):
+                    self.vy = +2
+            elif self.rect.centery < (HEIGHT / 2):
+                if (self.rect.left <= mouse_x <= self.rect.right and self.rect.top <= mouse_y <= self.rect.centery):
+                    self.vy = -2
+        
+        self.rect.move_ip(self.vx, self.vy)
+        if check_bound(self.rect) != (True, True):
+            self.kill() # 画面外にでたらGroupオブジェクトから消去される
 
 
 def main():
     pg.display.set_caption("はじめてのPygame")
-    screen = pg.display.set_mode((800, 600))
+    screen = pg.display.set_mode((WIDTH, HEIGHT))
+    bg_img = pg.image.load(f"fig/sabaku.jpg")
+
+    pins = pg.sprite.Group()
+
     clock = pg.time.Clock()
-    font = pg.font.Font(None, 80)
 
-    enn = pg.Surface((20, 20))
-    pg.draw.circle(enn, (255, 0, 0), (10, 10), 10)
-    enn.set_colorkey((0, 0, 0))
+    # 使いたい棒の座標タプルが入ったリストを指定する
+    pins.add(Yoko_Pin(2))
+    pins.add(Yoko_Pin(3))
+    pins.add(Tate_Pin(0))
 
-    tmr = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
         
-        txt = font.render(str(tmr), True, (255, 255, 255))
-        screen.fill((50, 50, 50))
-        screen.blit(txt, [300, 200])
-        screen.blit(enn, [100, 400])
+        screen.blit(bg_img, [0, 0])
+        
+        pins.update()
+        pins.draw(screen) 
         pg.display.update()
-        tmr += 1        
-        clock.tick(1)
+        clock.tick(60)
 
 
 if __name__ == "__main__":
