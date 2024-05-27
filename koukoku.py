2a58b4b594fead276109f8e8c05ec3073695f800
Yuichi <c0a230660d@edu.teu.ac.jp>
2024-05-28 03:55:18 +0900

    ゲームオーバー画面完了

--- koukoku.py
+++ koukoku.py
@@ -5,18 +5,25 @@ import time
 
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
+
 WIDTH, HEIGHT = 600,900
 
 
-def main():
+def main():  #main関数
     pg.display.set_caption("はじめてのPygame")
     screen = pg.display.set_mode((WIDTH, HEIGHT))
+    #screen1 = pg.display.set_mode((WIDTH,HEIGHT))
+    image = pg.image.load("fig/sougen.jpg")
+    #image = pg.transform.flip(image,True,False)
+    screen.blit(image,[150,400])
+    pg.display.update()
     clock = pg.time.Clock()
     font = pg.font.Font(None, 80)
 
     enn = pg.Surface((20, 20))
     pg.draw.circle(enn, (255, 0, 0), (10, 10), 10)
     enn.set_colorkey((0, 0, 0))
+    
     
     fonto = pg.font.Font(None,80)
     txt = fonto.render("Game Over",True,(255,0,0))   #GameOver表記
@@ -27,6 +34,7 @@ def main():
     
 
     tmr = 0
+    
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
@@ -35,6 +43,8 @@ def main():
         screen.fill((50, 50, 50))
         screen.blit(txt, [300, 200])
         screen.blit(enn, [100, 400])
+              
+        
         pg.display.update()
         tmr += 1        
         clock.tick(1)
