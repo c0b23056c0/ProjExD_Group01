7cfc4afe3cfd26d589d4809bb84b809eb7390da4
otake <c0a2302654@edu.teu.ac.jp>
2024-05-28 03:41:19 +0900

    クリア画面

--- koukoku.py
+++ koukoku.py
@@ -6,17 +6,17 @@ import time
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
 WIDTH = 600
-HEIGTH = 900
+HEIGTH = 800
 
-
+#クリアコード
 class Clear():
     def __init__(self, screen):
         font = pg.font.Font(None, 80)
         self.img = pg.image.load("fig/sora.jpg")
-        self.txt = font.render("CLEAR", True, (0, 0, 255))
-        #screen.fill(0, 255, 0)
+        self.txt = font.render("CLEAR", True, (255, 255, 0))
+        screen.fill((0, 255, 0))
         screen.blit(self.img, [0, 0])
-        screen.blit(self.txt, [200, 350])
+        screen.blit(self.txt, [200, 150])
         pg.display.update()
         time.sleep(1)
 
@@ -25,7 +25,7 @@ class Clear():
 
 def main():
     pg.display.set_caption("広告ゲーム")
-    screen = pg.display.set_mode((600, 900))
+    screen = pg.display.set_mode((600, 800))
     clock = pg.time.Clock()
     font = pg.font.Font(None, 80)
 
