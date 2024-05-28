96faed4aff480be7960fd48ef2deede0ba9a62cc
Yuichi <c0a230660d@edu.teu.ac.jp>
2024-05-28 16:14:32 +0900

    kanryou

--- koukoku.py
+++ koukoku.py
@@ -10,38 +10,12 @@ WIDTH, HEIGHT = 600,900 #サイズ
 def main():  #main関数
     pg.display.set_caption("はじめてのPygame")
     screen = pg.display.set_mode((WIDTH, HEIGHT))
-    #screen1 = pg.display.set_mode((WIDTH,HEIGHT))
-    image = pg.image.load("fig/sougen.jpg") #草原画像
-    #image = pg.transform.flip(image,True,False)
-    screen.blit(image,[0,0])
-    pg.display.update()
-    clock = pg.time.Clock()
-    font = pg.font.Font(None, 80)
-
-    enn = pg.Surface((20, 20))
-    pg.draw.circle(enn, (255, 0, 0), (10, 10), 10)
-    enn.set_colorkey((0, 0, 0))
-    
-    fonto = pg.font.Font(None,80)
-    txt = fonto.render("Game Over",True,(255,0,0))   #GameOver表記
-    screen.blit(txt,[WIDTH/2-150, HEIGHT/2])
+    image = pg.image.load("fig/game.png") # ゲームオーバー画像
+    image_rect = image.get_rect()
+    image_rect.center = WIDTH / 2, HEIGHT / 2
+    screen.blit(image, image_rect) 
     pg.display.update()
     time.sleep(5)
-    
-    tmr = 0
-    
-    while True:
-        for event in pg.event.get():
-            if event.type == pg.QUIT: return
-        
-        txt = font.render(str(tmr), True, (255, 255, 255))
-        screen.fill((50, 50, 50))
-        screen.blit(txt, [300, 200])
-        screen.blit(enn, [100, 400])
-        
-        pg.display.update()
-        tmr += 1        
-        clock.tick(1)
 
 
 if __name__ == "__main__":
